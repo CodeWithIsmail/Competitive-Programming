0e675e421ce71f7d8c4de70ca5f45f0d1dc2810d
CodeWithIsmail <hmismail360@gmail.com>
2024-01-31 20:42:50 +0600

    up

---
+++ Codeforces/C_Closest_Cities.cpp
@@ -0,0 +1,104 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Type:
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+int main()
+{
+    optimize();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, i, m, x, y;
+        cin >> n;
+        ll a[n + 1], case1[n + 1], case2[n + 1];
+        case1[0] = 0, case1[1] = 0, case1[2] = 1, case2[n] = 0, case2[n - 1] = 1;
+
+        for (i = 1; i <= n; i++)
+            cin >> a[i];
+
+        for (i = 2; i < n; i++)
+        {
+            if (a[i] - a[i - 1] < a[i + 1] - a[i])
+                case1[i + 1] = a[i + 1] - a[i];
+            else
+                case1[i + 1] = 1;
+        }
+        for (i = n - 1; i > 0; i--)
+        {
+            if (a[i] - a[i - 1] < a[i + 1] - a[i])
+                case2[i - 1] = 1;
+            else
+                case2[i - 1] = a[i] - a[i - 1];
+        }
+        for (i = n - 1; i >= 0; i--)
+            case2[i] += case2[i + 1];
+
+        for (i = 1; i <= n; i++)
+            case1[i] += case1[i - 1];
+
+        cin >> m;
+        while (m--)
+        {
+            cin >> x >> y;
+
+            if (x < y)
+                cout << case1[y] - case1[x];
+            else
+                cout << case2[y] - case2[x];
+            cout << "\n";
+        }
+    }
+}
