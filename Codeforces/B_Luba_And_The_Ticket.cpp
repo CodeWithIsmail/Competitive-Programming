bcc0091e895dff2fe6b4720abbc402e95ef1dce2
CodeWithIsmail <hmismail360@gmail.com>
2024-02-04 21:00:37 +0600

    up

---
+++ Codeforces/B_Luba_And_The_Ticket.cpp
@@ -0,0 +1,106 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Type:
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+string a;
+ll ch(string x)
+{
+    ll change = 0, sum1 = 0, sum2 = 0, i;
+    for (i = 0; i < 6; i++)
+    {
+        if (a[i] != x[i])
+            change++;
+        if (i < 3)
+            sum1 += x[i] - '0';
+        else
+            sum2 += x[i] - '0';
+    }
+    if (sum1 == sum2)
+        return change;
+    else
+        return -1;
+}
+int main()
+{
+    optimize();
+    cin >> a;
+    char i, j, k, l, m, n;
+    string x = "";
+    ll ans = INT_MAX;
+    for (i = '0'; i <= '9'; i++)
+    {
+        for (j = '0'; j <= '9'; j++)
+        {
+            for (k = '0'; k <= '9'; k++)
+            {
+                for (l = '0'; l <= '9'; l++)
+                {
+                    for (m = '0'; m <= '9'; m++)
+                    {
+                        for (n = '0'; n <= '9'; n++)
+                        {
+                            x.clear();
+                            x.pb(i), x.pb(j), x.pb(k), x.pb(l), x.pb(m), x.pb(n);
+                            ll tempAns = ch(x);
+                            if (tempAns != -1)
+                                ans = min(ans, tempAns);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ans;
+}
---
+++ Codeforces/D_Pair_of_Topics.cpp
@@ -0,0 +1,83 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// pbds
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+// Data Type:
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+int main()
+{
+    optimize();
+    ll n, i, ans = 0;
+    cin >> n;
+    ll a[n], b[n], dif[n];
+    for (i = 0; i < n; i++)
+        cin >> a[i];
+    for (i = 0; i < n; i++)
+        cin >> b[i];
+    for (i = 0; i < n; i++)
+        dif[i] = a[i] - b[i];
+    ordered_set st;
+    for (i = n - 1; i >= 0; i--)
+    {
+        ans += n - i - st.order_of_key(-dif[i] + 1) - 1;
+        st.insert(dif[i]);
+    }
+    cout << ans;
+}
