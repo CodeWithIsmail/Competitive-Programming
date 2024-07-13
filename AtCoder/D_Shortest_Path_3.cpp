66033a96f64080220938e3bf13e4bf1345bc7c2f
CodeWithIsmail <hmismail360@gmail.com>
2024-07-13 19:17:54 +0600

    AtCoder Beginner Contest 362 participated

---
+++ AtCoder/D_Shortest_Path_3.cpp
@@ -0,0 +1,112 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// typedef long long ll;
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Type:
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+const ll mx = 2e5 + 123;
+vector<pair<ll, ll>> adjacencyList[mx];
+ll shortestDistance[mx];
+ll self[mx];
+
+void dijkstra(ll source, ll totalNode)
+{
+    for (ll i = 0; i < totalNode; i++)
+        shortestDistance[i] = LONG_LONG_MAX;
+    shortestDistance[source] = self[0];
+
+    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+    pq.push({shortestDistance[source], source});
+    while (!pq.empty())
+    {
+        ll currentNode = pq.top().second;
+        ll currentCost = pq.top().first;
+        pq.pop();
+
+        if (currentCost > shortestDistance[currentNode])
+            continue;
+        for (auto x : adjacencyList[currentNode])
+        {
+            ll cost = x.second;
+            ll node = x.first;
+            if (currentCost + cost + self[node] < shortestDistance[node])
+            {
+                shortestDistance[node] = currentCost + cost + self[node];
+                pq.push({shortestDistance[node], node});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ll n, m, i;
+    cin >> n >> m;
+    for (i = 0; i < n; i++)
+        cin >> self[i];
+
+    for (i = 1; i <= m; i++)
+    {
+        ll u, v, w;
+        cin >> u >> v >> w;
+        adjacencyList[u - 1].push_back({v - 1, w});
+        adjacencyList[v - 1].push_back({u - 1, w});
+    }
+
+    dijkstra(0, n);
+    for (i = 1; i < n; i++)
+        cout << shortestDistance[i] << " ";
+}
