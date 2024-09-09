798533007f23b05a5c33e1bc4e4709e41bb1416f
CodeWithIsmail <hmismail360@gmail.com>
2024-09-09 12:49:10 +0600

    need a comeback

---
+++ Codeforces/E_Klee_s_SUPER_DUPER_LARGE_Array.cpp
@@ -0,0 +1,92 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Type:
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+int main()
+{
+    optimize();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, k;
+        cin >> n >> k;
+        ll sum = (n * (2 * k + n - 1)) / 2;
+        ll def = (k * (k - 1)) / 2;
+        ll low = k, up = k + n - 1, ans = LONG_LONG_MAX;
+        while (low <= up)
+        {
+            ll mid = (low + up) / 2;
+            ll leftsum = (mid * (mid + 1)) / 2 - def;
+            ll rightsum = sum - leftsum;
+            ll dif = abs(leftsum - rightsum);
+            if (mid == k)
+            {
+                ans = min(ans, dif);
+                up = mid - 1;
+                continue;
+            }
+            leftsum -= mid;
+            rightsum += mid;
+            ll dif2 = abs(leftsum - rightsum);
+            if (dif <= dif2)
+                low = mid + 1;
+            else
+                up = mid - 1;
+            ans = min(ans, dif);
+        }
+        cout << ans << "\n";
+    }
+}
