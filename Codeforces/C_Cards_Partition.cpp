b3b4924b00033d8e691a68534284a9e5c6f7f52a
CodeWithIsmail <hmismail360@gmail.com>
2024-09-27 22:19:55 +0600

    cf 975

---
+++ Codeforces/C_Cards_Partition.cpp
@@ -0,0 +1,125 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Type:
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+int main()
+{
+    optimize();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+
+        ll n, k, i, sum = 0, ans = 1;
+        cin >> n >> k;
+        ll a[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            sum += a[i];
+        }
+        ll mx = *max_element(a, a + n);
+        // cout << mx << " ";
+        for (i = 1; i <= n; i++)
+        {
+            ll perdeck = sum / i;
+            if (sum % i)
+                perdeck++;
+            perdeck = max(perdeck, mx);
+
+            if (perdeck >= mx)
+            {
+                ll extraneed = 1ll*i * perdeck - sum - k;
+                if (extraneed >= 0)
+                {
+                    ans = i;
+                }
+            }
+        }
+
+        // for (int i = n; i > 0; i--)
+        // {
+        //     // Calculate the quotient
+        //     long long quotient = (sum + k) / i;
+
+        //     // Check condition: quotient * i should not exceed the totalSum + k
+        //     if (quotient * i < sum)
+        //         continue;
+
+        //     // Check condition: quotient should not be greater than maxElement
+        //     if (quotient > mx)
+        //         continue;
+
+        //     // If both conditions are met, print the result and exit
+        //     // cout << i << endl;
+        //     ans = i;
+        //     if (ans != -1)
+        //         break;
+        // }
+
+        // // for (i = n; i > 0; i--)
+        // // {
+        // //     ll total = k + sum;
+        // //     ll perdeck = total / i;
+        // //     if (perdeck <= mx && perdeck * i >= sum)
+        // //     {
+        // //         ans = i;
+        // //         break;
+        // //     }
+        // // }
+
+        // cout << ans << "\n";
+    }
+}
