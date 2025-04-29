f086980d0bc81bbc6e7539fe1175a642b6208b7c
Mohammad Ismail Hossain <hmismail360@gmail.com>
2025-04-29 11:32:07 +0600

    add

---
+++ Codeforces/c.cpp
@@ -0,0 +1,105 @@
+///   ***   ---   ||         In the name of ALLAH        |||   ---   ***   ///
+
+///   ***   ---   ||       Author: Code_with_Ismail      |||   ---   ***   ///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Data Type:
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+// Shortcut:
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+#define YES printf("YES");
+#define Yes printf("Yes");
+#define NO printf("NO");
+#define No printf("No");
+
+// Const value:
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+// Optimize cin cout:
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// Fraction:
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+
+int main()
+{
+    optimize();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, k, i;
+        cin >> n >> k;
+        ll a[n], b[n];
+        for (i = 0; i < n; i++)
+            cin >> a[i];
+        set<ll> target;
+        for (i = 0; i < n; i++)
+        {
+            cin >> b[i];
+            if (b[i] >= 0)
+                target.insert(a[i] + b[i]);
+        }
+        if (target.size() == 0)
+        {
+            cout << *min_element(a, a + n) + k - *max_element(a, a + n) + 1;
+        }
+        else
+        {
+            if (target.size() > 1)
+                cout << 0;
+            else
+            {
+                ll tar = *target.begin();
+                bool ans = true;
+                for (i = 0; i < n; i++)
+                {
+                    ll need = tar - a[i];
+                    if (need > k || need < 0)
+                    {
+                        ans = false;
+                        break;
+                    }
+                }
+                if (ans)
+                    cout << 1;
+                else
+                    cout << 0;
+            }
+        }
+        cout << "\n";
+    }
+}
